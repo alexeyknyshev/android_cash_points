330d9501145ea01c75b45e7dd54c3e0b0db849d8
Alexey Knyshev <alexey.knyshev@gmail.com>
2015-06-25 13:18:50 +0300

    [C|BankList] Add: BankListModel which gets Bank list from sqlite [D|BankList] Add: Bank list sqlite db (bank.db) [D|BankList] Add: Bank list exported sql file (bank.sql)
    Signed-off-by: Alexey Knyshev <alexey.knyshev@gmail.com>

---
+++ banklistsqlmodel.cpp
@@ -0,0 +1,58 @@
+#include "banklistsqlmodel.h"
+
+#include <QSqlRecord>
+#include <QSqlQuery>
+
+#include <QDebug>
+
+BankListSqlModel::BankListSqlModel(QString connectionName)
+    : QSqlQueryModel(nullptr),
+      mConnectionName(connectionName)
+{
+    mRoleNames[IdRole]       = "bank_id";
+    mRoleNames[NameRole]     = "bank_name";
+    mRoleNames[UrlRole]      = "bank_url";
+    mRoleNames[TelRole]      = "bank_tel";
+    mRoleNames[TelDescrRole] = "bank_tel_description";
+
+    mRoleNames[TelDescrRole + 1] = "index";
+
+    setFilter("");
+}
+
+QHash<int, QByteArray> BankListSqlModel::roleNames() const
+{
+    return mRoleNames;
+}
+
+int BankListSqlModel::rowCount(const QModelIndex &parent) const
+{
+    return QSqlQueryModel::rowCount();
+}
+
+QVariant BankListSqlModel::data(const QModelIndex &item, int role) const
+{
+    if (role < Qt::UserRole)
+    {
+        return QSqlQueryModel::data(item, role);
+    }
+
+    if (role == TelDescrRole + 1) {
+        return item.row();
+    }
+
+    QSqlRecord rec = record(item.row());
+    return rec.value(role - Qt::UserRole).toString();
+}
+
+void BankListSqlModel::setFilter(QString filterStr)
+{
+    filterStr.replace('_', "");
+    filterStr.replace('%', "");
+    filterStr.replace('*', '%');
+    filterStr.replace('?', '_');
+    mQueryMask = "SELECT id, name, url, tel, tel_description FROM banks WHERE name LIKE '%" + filterStr +
+                 "%' or url LIKE '%" + filterStr + "%' or tel LIKE '%" + filterStr + "%'";
+    setQuery(mQueryMask, QSqlDatabase::database(mConnectionName));
+}
+
---
+++ banklistsqlmodel.h
@@ -0,0 +1,40 @@
+#ifndef BANKLISTSQLMODEL_H
+#define BANKLISTSQLMODEL_H
+
+#include <QtSql/QSqlQueryModel>
+
+#include <QSqlQuery>
+
+class BankListSqlModel : public QSqlQueryModel
+{
+    Q_OBJECT
+
+public:
+    enum Roles {
+        IdRole = Qt::UserRole,
+        NameRole,
+        UrlRole,
+        TelRole,
+        TelDescrRole
+    };
+
+    explicit BankListSqlModel(QString connectionName);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &item, int role) const override;
+
+public slots:
+    void setFilter(QString filterStr);
+
+protected:
+    QHash<int, QByteArray> roleNames() const override;
+
+private:
+    QHash<int, QByteArray> mRoleNames;
+    QString mQueryMask;
+    const QString mConnectionName;
+    QSqlQuery mQuery;
+};
+
+#endif // BANKLISTSQLMODEL_H
