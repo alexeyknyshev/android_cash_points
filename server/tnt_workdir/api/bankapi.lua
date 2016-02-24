57fbd24acf94dd49590b1f6988ded9c0b96433ac
Alexey Knyshev <alexey.knyshev@gmail.com>
2016-02-24 19:08:11 +0300

    [C|Server] BankApi: Use col constants as tuple indexes insted magic numbers
    Signed-off-by: Alexey Knyshev <alexey.knyshev@gmail.com>

--- server/tnt_workdir/api/bankapi.lua
+++ server/tnt_workdir/api/bankapi.lua
@@ -1,5 +1,14 @@
 json = require('json')
 
+local COL_BANK_ID = 1
+local COL_BANK_NAME = 2
+local COL_BANK_NAME_TR = 3
+local COL_BANK_NAME_TR_ALT = 4
+local COL_BANK_PARTNERS = 5
+local COL_BANK_LICENCE = 7
+local COL_BANK_RATING = 8
+local COL_BANK_TEL = 9
+
 local MAX_BANKS_BATCH_SIZE = 256
 
 local function _getBankById(bankId)
@@ -10,21 +19,21 @@ local function _getBankById(bankId)
 
     t = t[1]
 
-    local partnersTuple = t[5]
+    local partnersTuple = t[COL_BANK_PARTNERS]
     local partners = {}
     for i = 1, #partnersTuple do
         partners[#partners + 1] = partnersTuple[i]
     end
 
     local bank = {
-        id = t[1],
-        name = t[2],
-        name_tr = t[3],
-        name_tr_alt = t[4],
+        id = t[COL_BANK_ID],
+        name = t[COL_BANK_NAME],
+        name_tr = t[COL_BANK_NAME_TR],
+        name_tr_alt = t[COL_BANK_NAME_TR_ALT],
         partners = partners,
-        licence = t[7],
-        rating = t[8],
-        tel = t[9],
+        licence = t[COL_BANK_LICENCE],
+        rating = t[COL_BANK_RATING],
+        tel = t[COL_BANK_TEL],
     }
 
     return bank
@@ -35,21 +44,20 @@ function getBankById(bankId)
     if bank then
         return json.encode(bank)
     end
+
+    return ""
 end
 
 function getBanksBatch(reqJson)
     local req = json.decode(reqJson)
     if not req or not req.banks then
         box.error{ code = 400, reason = "getBanksBatch: malformed request" }
-        return nil
+        return ""
     end
 
     local result = {}
     for _, bankId in pairs(req.banks) do
-        local bank = _getBankById(bankId)
-        if bank then
-            result[#result + 1] = bank
-        end
+        result[#result + 1] = _getBankById(bankId)
         if #result == MAX_BANKS_BATCH_SIZE then
             break
         end
@@ -63,7 +71,7 @@ function getBanksList()
 
     local result = {}
     for _, tuple in ipairs(t) do
-        result[#result + 1] = tuple[1]
+        result[#result + 1] = tuple[COL_BANK_ID]
     end
 
     return json.encode(setmetatable(result, { __serialize = "seq" }))
