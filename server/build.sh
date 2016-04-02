dd2065e2873959b9ed066c095e4e94c00ddeeff8
Alexey Knyshev <alexey.knyshev@gmail.com>
2016-04-02 21:40:03 +0300

    [T|Server] Disable building outdated go pkgs
    Signed-off-by: Alexey Knyshev <alexey.knyshev@gmail.com>

--- server/build.sh
+++ server/build.sh
@@ -6,14 +6,14 @@ DATE=`date -u '+%Y/%m/%dT%H:%M:%S'`
 export GOPATH="$SCRIPT_DIR"
 #go get
 go build github.com/alexeyknyshev/cpsrv
-go build github.com/alexeyknyshev/server
-go build github.com/alexeyknyshev/tools/server_sqlite_to_redis
+#go build github.com/alexeyknyshev/server
+#go build github.com/alexeyknyshev/tools/server_sqlite_to_redis
 go build github.com/alexeyknyshev/tools/server_sqlite_to_tarantool
 go install github.com/alexeyknyshev/cpsrv
-go install github.com/alexeyknyshev/server
-go install github.com/alexeyknyshev/tools/server_sqlite_to_redis
+#go install github.com/alexeyknyshev/server
+#go install github.com/alexeyknyshev/tools/server_sqlite_to_redis
 go install github.com/alexeyknyshev/tools/server_sqlite_to_tarantool
 [ -e "$SCRIPT_DIR/cpsrv" ] && rm "$SCRIPT_DIR/cpsrv"
-[ -e "$SCRIPT_DIR/server" ] && rm "$SCRIPT_DIR/server"
-[ -e "$SCRIPT_DIR/server_sqlite_to_redis" ] && rm "$SCRIPT_DIR/server_sqlite_to_redis"
+#[ -e "$SCRIPT_DIR/server" ] && rm "$SCRIPT_DIR/server"
+#[ -e "$SCRIPT_DIR/server_sqlite_to_redis" ] && rm "$SCRIPT_DIR/server_sqlite_to_redis"
 [ -e "$SCRIPT_DIR/server_sqlite_to_tarantool" ] && rm "$SCRIPT_DIR/server_sqlite_to_tarantool"
