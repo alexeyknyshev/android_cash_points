000caa170278160f640997267fada14135080924
cloudedcat <cloudedcat@ya.ru>
2016-04-03 17:38:55 +0300

    Added test for existing checkpoint patch

--- server/src/github.com/alexeyknyshev/cpsrv/cpsrv_patch_test.go
+++ server/src/github.com/alexeyknyshev/cpsrv/cpsrv_patch_test.go
@@ -13,7 +13,8 @@ import (
 	//"net/http"
 	//"net/http/httptest"
 	//"sort"
-	//"strconv"
+	"reflect"
+	"strconv"
 	"testing"
 )
 
@@ -71,51 +72,30 @@ func getPatchExampleNewCP() *PatchRequest {
 }
 
 func getPatchExampleExistCP() *PatchRequest {
-	False := false
-	True := true
 	patchReq := PatchRequest{
-		Longitude:      37.6878262,
-		Latitude:       55.6946643,
-		Type:           "atm",
-		BankId:         322,
-		TownId:         4,
-		Address:        "г. Москва, Район Моей Мечты",
-		AddressComment: "ОАО UnderButtom",
-		MetroName:      "",
-		FreeAccess:     &True,
-		MainOffice:     &False,
-		WithoutWeekend: &False,
-		RoundTheClock:  &False,
-		WorksAsShop:    &True,
-		Schedule:       Schedule{},
-		Tel:            "",
-		Additional:     "",
-		Rub:            &True,
-		Usd:            &False,
-		Eur:            &False,
-		CashIn:         &False,
+		Id:     58552,
+		BankId: 2764,
 	}
 	return &patchReq
 
 }
 
-func TestPatchCreating(t *testing.T) {
+func TestPatchCreateNewCP(t *testing.T) {
 
 	type Req struct {
 		Data   PatchRequest `json:"data"`
 		UserId uint         `json:"user_id"`
 	}
 	patchReq := getPatchExampleNewCP()
-	fmt.Println("PatchReq.Eur = ", *(patchReq.Eur))
 	request := Req{
 		Data:   *patchReq,
 		UserId: 0,
 	}
 	requestJson, err := json.Marshal(request)
-	fmt.Print("\n\n", string(requestJson), "\n\n")
 	if err != nil {
 		t.Fatalf("Json Marshal error %v", err)
 	}
+	fmt.Print("Request:\n", string(requestJson), "\n")
 
 	hCtx, err := makeHandlerContext(getServerConfig())
 	if err != nil {
@@ -139,7 +119,7 @@ func TestPatchCreating(t *testing.T) {
 
 	resp, err := hCtx.Tnt().Call("getCashpointPatches", []interface{}{CpId})
 	if err != nil {
-		t.Errorf("Failed to unmarshal %v", err)
+		t.Errorf("Failed call tarantool getCashpointPatches func %v", err)
 	}
 	fmt.Print("\nCheck new patch, CpId :\n ", resp)
 
@@ -147,6 +127,75 @@ func TestPatchCreating(t *testing.T) {
 
 }
 
-func func_name() {
+func TestPatchChangeExistCP(t *testing.T) {
 
+	type Req struct {
+		Data   PatchRequest `json:"data"`
+		UserId uint         `json:"user_id"`
+	}
+	patchReq := getPatchExampleExistCP()
+	request := Req{
+		Data:   *patchReq,
+		UserId: 1,
+	}
+	requestJson, err := json.Marshal(request)
+	if err != nil {
+		t.Fatalf("Json Marshal error %v", err)
+	}
+	fmt.Print("Request:\n", string(requestJson), "\n")
+
+	hCtx, err := makeHandlerContext(getServerConfig())
+	if err != nil {
+		t.Fatalf("Connection to tarantool failed: %v", err)
+	}
+	defer hCtx.Close()
+
+	metrics, err := getSpaceMetrics(hCtx)
+	if err != nil {
+		t.Errorf("Failed to get space metric on start: %v", err)
+	}
+	defer checkSpaceMetrics(t, func() ([]byte, error) { return getSpaceMetrics(hCtx) }, metrics)
+
+	TaranResp, err := hCtx.Tnt().Call("cashpointProposePatch", []interface{}{requestJson})
+	if err != nil {
+		t.Errorf("Failed to create patch: %v", err)
+		return
+	}
+	CpId := TaranResp.Data[0].([]interface{})[0]
+	fmt.Print(reflect.TypeOf(CpId), " Volume = ", CpId)
+	if CpId == uint64(0) {
+		t.Error("Failed to create patch, CpId == 0")
+		return
+	} else {
+		fmt.Print("\nCreate new patch, CpId = ", CpId)
+	}
+
+	CPPatchesTaranResp, err := hCtx.Tnt().Call("getCashpointPatches", []interface{}{CpId})
+	if err != nil {
+		t.Errorf("Failed to unmarshal %v", err)
+	}
+	CPPatchesJson := CPPatchesTaranResp.Data[0].([]interface{})[0].(string)
+
+	fmt.Print("\nPatches of cashpoint №", CpId, ":\n", CPPatchesJson, "\n")
+	CPPatchesJsonByte := []byte(CPPatchesJson)
+	var CPPatches map[string]interface{}
+	err = json.Unmarshal(CPPatchesJsonByte, &CPPatches)
+	if err != nil {
+		t.Errorf("Patches unmarshal err: %v", err)
+	}
+
+	last_key := int64(0)
+	//Search last patch number
+	for key := range CPPatches {
+		int_key, _ := strconv.ParseInt(key, 10, 64)
+		if int_key > last_key {
+			last_key = int_key
+		}
+	}
+
+	fmt.Println("last patch id = ", last_key)
+	_, err = hCtx.Tnt().Call("_deleteCashpointPatchById", []interface{}{last_key})
+	if err != nil {
+		t.Errorf("P_deleteCashpointPatchById err: %v", err)
+	}
 }
