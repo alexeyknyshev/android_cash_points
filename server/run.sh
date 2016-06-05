bd6dd8d3dea06772e3e00dacd708cefbe357e36e
Alexey Knyshev <alexey.knyshev@gmail.com>
2016-06-05 19:43:06 +0300

    [C|Server] Fix: run.sh arg parsing

--- server/run.sh
+++ server/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-declare -a args
+test_arg="--test"
 test=false
 for i in $*; do
-	[ $i == "--test" ] && test=true || args[${#args}]=$i
+	[ $i == $test_arg ] && test=true
 done
+
+args=${*/$test_arg}
+
 if $test ; then
 	docker/generator.awk docker/dockerfile_cpsrv_template --test > docker/dockerfile_cpsrv
 else
 	docker/generator.awk docker/dockerfile_cpsrv_template > docker/dockerfile_cpsrv
 fi
 set -x
-docker-compose -p cashpoints up  $args
+docker-compose -p cashpoints up $args
 
